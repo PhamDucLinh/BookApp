078b3e8e0be3bdfbaf9371d6d7037e0be26246d5
Stephen Linh <linh171720012001@gmail.com>
2023-03-13 22:26:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bookapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.bookapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.bookapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        //handle login click, start login screen
+        binding.LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+        //handle login click, start continue without login  screen
+        binding.skipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DashboardUserActivity.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/bookapp/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.bookapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        //Start main screen after 2s
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // start main screen
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+                //finish this activyty
+            }
+        }, 2000); //2000 = 2s
+    }
+}
