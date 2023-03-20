a862106bbd4e22f781bf1c043d4cb657a251a769
Stephen Linh <linh171720012001@gmail.com>
2023-03-20 21:23:30 +0700

    21:23 20/3/2023

--- app/src/main/java/com/example/bookapp/DashboardAdminActivity.java
+++ app/src/main/java/com/example/bookapp/DashboardAdminActivity.java
@@ -2,13 +2,53 @@ package com.example.bookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.bookapp.databinding.ActivityDashboardAdminBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class DashboardAdminActivity extends AppCompatActivity {
+
+    //view binding
+    private ActivityDashboardAdminBinding binding;
+
+    //xac thuc firebase
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard_admin2);
+        binding = ActivityDashboardAdminBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        chekUser();
+
+        // xu li click log out
+        binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                chekUser();
+            }
+        });
+    }
+
+    private void chekUser() {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser == null) {
+            //ko login quay lai main screen
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        } else {
+            //login thanh cong lay thong tin user
+            String email = firebaseUser.getEmail();
+            //set in text view  of toolbar
+            binding.subTitleTv.setText(email);
+        }
     }
 }
--- app/src/main/java/com/example/bookapp/DashboardUserActivity.java
+++ app/src/main/java/com/example/bookapp/DashboardUserActivity.java
@@ -2,13 +2,52 @@ package com.example.bookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.bookapp.databinding.ActivityDashboardUserBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class DashboardUserActivity extends AppCompatActivity {
+
+    //view binding
+    private ActivityDashboardUserBinding binding;
+
+    //xac thuc firebase
+    private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard_user);
+        binding = ActivityDashboardUserBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUser();
+
+        // xu li click log out
+        binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                checkUser();
+            }
+        });
+    }
+    private void checkUser() {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser == null) {
+            //ko login quay lai main screen
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        } else {
+            //login thanh cong lay thong tin user
+            String email = firebaseUser.getEmail();
+            //set in text view  of toolbar
+            binding.subTitleTv.setText(email);
+        }
     }
 }
