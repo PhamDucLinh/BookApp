25547a11f0d805bef8aee481ee4b23bfa777b1d1
Stephen Linh <linh171720012001@gmail.com>
2023-04-04 15:02:38 +0700

    21:23 20/3/2023

--- app/src/main/java/com/example/bookapp/DashboardAdminActivity.java
+++ app/src/main/java/com/example/bookapp/DashboardAdminActivity.java
@@ -1,5 +1,6 @@
 package com.example.bookapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,6 +10,13 @@ import android.view.View;
 import com.example.bookapp.databinding.ActivityDashboardAdminBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class DashboardAdminActivity extends AppCompatActivity {
 
@@ -17,6 +25,10 @@ public class DashboardAdminActivity extends AppCompatActivity {
 
     //xac thuc firebase
     private FirebaseAuth firebaseAuth;
+
+    //tao mang de luu category
+    private ArrayList<ModelCategory> categoryArrayList;
+    private AdapterCategory adapterCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +39,47 @@ public class DashboardAdminActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
         chekUser();
-
+        loadCategories();
         // xu li click log out
         binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 firebaseAuth.signOut();
                 chekUser();
+
+
+            }
+
+            private void loadCategories() {
+
+                categoryArrayList = new ArrayList<>();
+
+                // lay all data tu firebase den categories
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Categories");
+                ref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange( DataSnapshot snapshot) {
+                        //clear data trc khi add
+                        categoryArrayList.clear();
+                        for (DataSnapshot ds : snapshot.getChildren()) {
+
+                            //add data
+                            ModelCategory model = ds.getValue(ModelCategory.class);
+                            //add vo mang
+                            categoryArrayList.add(model);
+                        }
+                        //setup adapter
+                        adapterCategory = new AdapterCategory(DashboardAdminActivity.this, categoryArrayList);
+                        //set adapter vo recycleview
+                        binding.categoriesRv.setAdapter(adapterCategory);
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
         });
         //xu li click add category
@@ -43,8 +89,47 @@ public class DashboardAdminActivity extends AppCompatActivity {
                 startActivity(new Intent(DashboardAdminActivity.this, CategoryAddActivity.class));
             }
         });
+
+        //xu li click  add pdf
+        binding.addPdfFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DashboardAdminActivity.this, PdfAddActivity.class));
+            }
+        });
     }
- 
+
+    private void loadCategories() {
+
+        categoryArrayList = new ArrayList<>();
+
+        // lay all data tu firebase den categories
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Categories");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange( DataSnapshot snapshot) {
+                //clear data trc khi add
+                categoryArrayList.clear();
+                for (DataSnapshot ds : snapshot.getChildren()) {
+
+                    //add data
+                    ModelCategory model = ds.getValue(ModelCategory.class);
+                    //add vo mang
+                    categoryArrayList.add(model);
+                }
+                //setup adapter
+                adapterCategory = new AdapterCategory(DashboardAdminActivity.this, categoryArrayList);
+                //set adapter vo recycleview
+                binding.categoriesRv.setAdapter(adapterCategory);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
 
 
     private void chekUser() {
